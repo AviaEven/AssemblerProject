921c702764c08c90ba53eb965fa17251c4fff2f4
AviaEven <154089921+AviaEven@users.noreply.github.com>
2023-12-20 11:52:43 +0200

    preprocessor.c

--- preprocessor.c
+++ preprocessor.c
@@ -1,7 +1,7 @@
 
 #include "preprocessor.h"
-#include "../data_structures/vector/vector.h"
-#include "../data_structures/trie/trie.h"
+#include "vector.h"
+#include "trie.h"
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
